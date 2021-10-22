c4f4063389550864432b54384730e726042f97eb
Juris Plocins <juris.plocins@icloud.com>
2021-10-22 20:25:28 +0300

    Adding Stepper component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,71 @@
 <template>
+  <div class="container">
+
+    <div class="stepper">
+      <div class="stepper-header">
+
+        <div class="step-header">
+
+          <div class="step-content">
+            <div class="step-icon">
+              <div class="step-icon-state">
+                <div class="step-icon-label-text">1</div>
+              </div>
+            </div>
+
+            <div class="step-label">
+              <div class="step-label-text">Personal info</div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="step-header">
+          <div class="step-horizontal-line"></div>
+          <div class="step-content">
+            <div class="step-icon">
+              <div class="step-icon-state">
+                <div class="step-icon-label-text">2</div>
+              </div>
+            </div>
+
+            <div class="step-label">
+              <div class="step-label-text">Member ship</div>
+            </div>
+          </div>
+
+
+        </div>
+
+        <div class="step-header">
+          <div class="step-horizontal-line"></div>
+
+          <div class="step-content">
+            <div class="step-icon">
+              <div class="step-icon-state">
+                <div class="step-icon-label-text">3</div>
+              </div>
+            </div>
+
+            <div class="step-label">
+              <div class="step-label-text">Overview</div>
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
+      <!-- :: end stepper-header -->
+
+      <div class="stepper-content">
+        stepper content
+      </div>
+    </div>
+
+    <!-- :: end stepper -->
+  </div>
+
 </template>
 <script>
 
